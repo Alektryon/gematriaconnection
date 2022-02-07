43bd682f9cf3e9f0b45c2f1c33579e02edd0e1ae
Alektryon <84470810+Alektryon@users.noreply.github.com>
2022-02-07 14:40:58 +0000

    Update gematriaNGG.js

--- gematriaNGG.js
+++ gematriaNGG.js
@@ -448,6 +448,16 @@ class cipher {
 				this.vArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12, 13,14,15,16, 17, 18, 19, 20, 20, 21, 22, 23, 24]
 				this.vArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12, 13,14,15,16, 17, 18, 19, 20, 20, 21, 22, 23, 24]
 				break;
+
+					case "Elizabethan Reverse":
+				for (y = 0; y < 26; y++) {
+					this.cArr[y] = (y + 97)
+					this.cArr2[y] = (y + 65)
+				}
+				this.vArr = [24,23,22,21,20,19,18,17,16,16,15,14,13,12,11,10,9,8,7,6,5,5,4,3,2,1]
+				this.vArr2 = [24,23,22,21,20,19,18,17,16,16,15,14,13,12,11,10,9,8,7,6,5,5,4,3,2,1]
+				break;
+
 				
 					case "Elizabethan Short":
 				for (y = 0; y < 26; y++) {
@@ -1163,13 +1173,13 @@ function Build_Ciphers() {
             case "Novem Maximus": allCiphers[allCiphers.length] = new cipher(key, "Novem Maximus", 166, 199, 66,); break;
                 
 				// Elizabethan Ciphers
-			case "Elizabethan Simple": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Simple", 210, 240, 55,); break;
+			case "Elizabethan Simple": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Simple", 80, 235, 21); break;
 				
-			case "Elizabethan Simple Reversed": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Simple", 55,255 ,210 ,"Reverse"); break;
+			case "Elizabethan Reverse": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Reverse", 0, 186, 0); break;
 				
-			case "Elizabethan Short": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Short", 250, 88, 66,); break;
+			case "Elizabethan Short": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Short", 100, 216, 209); break;
 				
-			case "Elizabethan Kaye": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Kaye", 88, 77, 99,); break;
+			case "Elizabethan Kaye": allCiphers[allCiphers.length] = new cipher(key, "Elizabethan Kaye", 220, 93, 73); break;
 		}
 	}
 
@@ -1300,7 +1310,7 @@ function Set_Categories() {
 	cipherArray["Reverse Arabic Ordinal"] = "Arabic Alphabet"
 	
 	cipherArray["Elizabethan Simple"] = "Elizabethan"
-	cipherArray["Elizabethan Simple Reversed"] = "Elizabethan"
+	cipherArray["Elizabethan Reverse"] = "Elizabethan"
 	cipherArray["Elizabethan Short"] = "Elizabethan"
 	cipherArray["Elizabethan Kaye"] = "Elizabethan"
 	
