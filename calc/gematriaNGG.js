1a996fbf0be911172e99564049e038bbfc063b6e
Alektryon <84470810+Alektryon@users.noreply.github.com>
2022-06-16 13:37:27 +0100

    Add files via upload

--- calc/gematriaNGG.js
+++ calc/gematriaNGG.js
@@ -341,6 +341,12 @@ class cipher {
 				this.vArr2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18,19,20,21,22,23,24,25,26,27,28,29,29,30,31,32,33]
                 }
 				break;
+			case "SatanAQ":
+				for (y = 0; y < 62; y++) {
+				this.cArr = [48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
+				this.vArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]
+				}
+				break;
 			case "Abrahadabra":
 				for (y = 0; y < 26; y++) {
 					this.cArr[y] = (y + 97)
@@ -1117,6 +1123,7 @@ function Build_Ciphers() {
 			case "Keypad": allCiphers[allCiphers.length] = new cipher(key, "Keypad", 255, 126, 255); break;
 			case "Isisian Codes": allCiphers[allCiphers.length] = new cipher(key, "Isisian Codes", 220,255,103); break;
 			case "Mirror": allCiphers[allCiphers.length] = new cipher(key, "Mirror", 210,220,240); break;
+			case "Satanic Qabbala": allCiphers[allCiphers.length] = new cipher(key, "SatanAQ", 223,98,64); break;
 
 			case "NonPrime Numbers": allCiphers[allCiphers.length] = new cipher(key, "NonPrime", 125, 66, 244,); break;
 			case "Reverse NonPrime Numbers": allCiphers[allCiphers.length] = new cipher(key, "NonPrime", 125, 100, 244,"Reverse"); break;
@@ -1260,6 +1267,7 @@ function Set_Categories() {
 	cipherArray["Isisian Codes"] = "Other"
 	cipherArray["Mirror"] = "Other"
  	cipherArray["Novem Maximus"] = "Other"
+ 	cipherArray["Satanic Qabbala"] = "Other"
         
 	cipherArray["Qwerty"] = "Keyboard"
 	cipherArray["Reverse Qwerty"] = "Keyboard"
@@ -1357,7 +1365,7 @@ function Add_AllCiphers(impBool = false) {
 }
 function Add_BaseCiphers(impBool = false) {
 	var x, q, cN, z
-	var baseCiphers = ["English Ordinal", "Full Reduction", "English Extended", "Alphanumeric Qabbala"]
+	var baseCiphers = ["English Ordinal", "Full Reduction", "Reverse Ordinal", "Reverse Full Reduction", "Alphanumeric Qabbala"]
 
 	openCiphers = []
 	for (z = 0; z < allCiphers.length; z++) {
